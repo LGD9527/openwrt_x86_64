6fbb81458d0884be4613d907cacc3aed78631486
Albrt9527 <2563009889@qq.com>
2021-06-07 10:25:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,9 +14,9 @@
 #sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 # 清除旧版argon主题并拉取最新版
-#pushd package/lean
-#rm -rf luci-theme-argon
-#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon luci-theme-argon
+pushd package/lean
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon luci-theme-argon
 
 # 更改主题
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ../feeds/luci/collections/luci/Makefile
@@ -29,7 +29,7 @@
 #sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\/thermal_zone0\/temp` \/ 1000") or "?"%> \&#8451; ) /g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 
 # Add ServerChan
-#git clone --depth=1 https://github.com/tty228/luci-app-serverchan
+git clone --depth=1 https://github.com/tty228/luci-app-serverchan
 
 #Add luci-app-adguardhome
 #svn co https://github.com/Lienol/openwrt/tree/main/package/diy/luci-app-adguardhome
