cc7cfb2d60d77628d899038a86ff1b889a24b9ca
Albrt9527 <2563009889@qq.com>
2021-06-09 12:43:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -29,10 +29,10 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon luci-theme-argo
 #sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\/thermal_zone0\/temp` \/ 1000") or "?"%> \&#8451; ) /g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 
 # openclash
-git clone --depth=1 https://github.com/vernesong/OpenClash
+git clone --depth=1 https://github.com/vernesong/OpenClash.git
 
 # Add ServerChan
-git clone --depth=1 https://github.com/tty228/luci-app-serverchan
+git clone --depth=1 https://github.com/tty228/luci-app-serverchan.git
 
 #Add luci-app-adguardhome
 #svn co https://github.com/Lienol/openwrt/tree/main/package/diy/luci-app-adguardhome
